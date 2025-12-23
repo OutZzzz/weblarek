91c569930cfe612e20babc2dd2661db79c7b16d4
Alexey Sudakov <outzz@yandex.ru>
2025-12-23 14:50:52 +0300

    Добавлен View класс basket

---
+++ src/components/views/Basket.ts
@@ -0,0 +1,47 @@
+import { IProduct } from "../../types"
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+
+interface ICardActions {
+    onClick: () => {}
+}
+
+type TBasket = {
+    order: HTMLElement[];
+    totalPrice: number
+}
+
+export class Basket extends Component<TBasket> {
+    protected orderListElement: HTMLElement;
+    protected totalPriceElement: HTMLElement;
+    protected buttonElement: HTMLElement;
+
+    constructor(container: HTMLElement, actions?: ICardActions) {
+        super(container);
+
+        this.orderListElement = ensureElement<HTMLElement>(
+            '.basket__list',
+            this.container
+        )
+        this.totalPriceElement = ensureElement<HTMLElement>(
+            '.basket__price', 
+            this.container
+        )
+        this.buttonElement = ensureElement<HTMLElement>(
+            '.basket__button',
+            this.container
+        )
+
+        if (actions?.onClick) {
+            this.buttonElement.addEventListener('click', actions.onClick)
+        }
+    }
+
+    set order(value: HTMLElement[]) {
+        this.orderListElement.append(...value)
+    }
+
+    set totalPrice(value: number) {
+        this.totalPriceElement.textContent = String(value)
+    }
+}
