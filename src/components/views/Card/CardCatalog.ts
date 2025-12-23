3f91c13380b04f6791efbd342b298c07463ba468
Alexey Sudakov <outzz@yandex.ru>
2025-12-23 14:53:33 +0300

    feat: добавлен view класс cardCatalog

---
+++ src/components/views/Card/CardCatalog.ts
@@ -0,0 +1,57 @@
+import { IProduct } from "../../../types";
+import { categoryMap } from "../../../utils/constants";
+import { ensureElement } from "../../../utils/utils";
+import { Card } from "./Card";
+
+interface ICardActions {
+    onClick: () => {}
+}
+
+type CategoryKey = keyof typeof categoryMap;
+/* export type TCardCatalog = Pick<IProduct, 'image' | 'category'>; */
+type TCardCatalog = {
+    category: string,
+    image: string,
+    title: string,
+    price: number | null
+}
+
+
+export class CardCatalog extends Card<TCardCatalog> {
+    protected imageElement: HTMLImageElement;
+    protected categoryElement: HTMLElement;
+
+    constructor(container: HTMLElement, actions?: ICardActions) {
+        super(container);
+
+        this.categoryElement = ensureElement<HTMLElement>(
+            '.card__category',
+            this.container
+        );
+
+        this.imageElement = ensureElement<HTMLImageElement>(
+            '.card__image',
+            this.container
+        );
+
+        if (actions?.onClick) {
+            this.container.addEventListener('click', actions.onClick);
+        }
+    }
+
+    set category(value: string) {
+        this.categoryElement.textContent = value;
+
+        for (const key in categoryMap) {
+            this.categoryElement.classList.toggle(
+                categoryMap[key as CategoryKey],
+                key === value
+            );
+        }
+    }
+
+    set image(value: string) {
+        this.setImage(this.imageElement, value, this.title);
+    }
+
+}
