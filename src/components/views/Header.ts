f9265f292bd3bc9192c5b6976c0601f76851b958
Alexey Sudakov <outzz@yandex.ru>
2025-12-25 12:17:01 +0300

    fix: поправил оформление

--- src/components/views/Header.ts
+++ src/components/views/Header.ts
@@ -13,8 +13,14 @@ export class Header extends Component<IHeader> {
 	constructor(protected events: IEvents, container: HTMLElement) {
 		super(container);
 		
-		this.counterElement = ensureElement<HTMLElement>('.header__basket-counter', this.container);
-		this.basketButton = ensureElement<HTMLButtonElement>('.header__basket', this.container);
+		this.counterElement = ensureElement<HTMLElement>(
+			'.header__basket-counter', 
+			this.container
+		);
+		this.basketButton = ensureElement<HTMLButtonElement>(
+			'.header__basket', 
+			this.container
+		);
 		
 		this.basketButton.addEventListener('click', () => {
 			this.events.emit('basket:open');
